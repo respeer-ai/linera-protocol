7c84fc8bc5b165423f1150dc8486fb8be66164cf
hong-t <th13564039482@163.com>
2025-01-23 15:35:50 +0000

    retry

--- linera-core/src/updater.rs
+++ linera-core/src/updater.rs
@@ -257,8 +257,6 @@ where
             blob_ids.remove(&blob.id()); // Keep only blobs we may need to resend.
         }
 
-        let mut retries = 0;
-
         loop {
             match self
                 .remote_node
@@ -268,21 +266,13 @@ where
                 Ok(info) => return Ok(info),
                 Err(NodeError::MissingCrossChainUpdate { .. })
                 | Err(NodeError::InactiveChain(_))
-                    if !sent_cross_chain_updates && retries < 10 =>
+                    if !sent_cross_chain_updates =>
                 {
                     sent_cross_chain_updates = true;
                     // Some received certificates may be missing for this validator
                     // (e.g. to create the chain or make the balance sufficient) so we are going to
                     // synchronize them now and retry.
-                    info!("Send cross chain updates {} retries {}", chain_id, retries);
-                    retries += 1;
-                    match self.send_chain_information_for_senders(chain_id).await {
-                        Ok(_) => {},
-                        Err(e) => {
-                            warn!("Failed send chain information {}", chain_id);
-                            return Err(e);
-                        }
-                    }
+                    self.send_chain_information_for_senders(chain_id).await?;
                 }
                 Err(NodeError::BlobNotFoundOnRead(_)) if !blob_ids.is_empty() => {
                     // For `BlobNotFoundOnRead`, we assume that the local node should already be
