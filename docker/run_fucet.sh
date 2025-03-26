86977b4395f7f02b95a64a70c0f2ddfa02bfc1f1
chen <531759321@qq.com>
2025-03-26 14:51:48 +0800

    Fix docker build

--- docker/run_fucet.sh
+++ docker/run_fucet.sh
@@ -57,28 +57,21 @@ if [ "x$COMPILE" = "x1" ]; then
   cp $SCRIPT_DIR/faucet-entrypoint.sh docker/
 
   git checkout $GIT_COMMIT
-  # Get latest commit to avoid compilation for the same version
-  LATEST_COMMIT=`git rev-parse HEAD`
-  LATEST_COMMIT=${LATEST_COMMIT:0:10}
-  INSTALLED_COMMIT=`linera --version | grep tree | awk -F '/' '{print $7}'`
 
-  if [ "x$LATEST_COMMIT" != "x$INSTALLED_COMMIT" ]; then
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        docker build --build-arg git_commit="$LATEST_COMMIT" -f $SCRIPT_DIR/Dockerfile.faucet . -t linera-faucet || exit 1
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        CPU_ARCH=$(sysctl -n machdep.cpu.brand_string)
-        if [[ "$CPU_ARCH" == *"Apple"* ]]; then
-            docker build --build-arg git_commit="$LATEST_COMMIT" --build-arg target=aarch64-unknown-linux-gnu -f $SCRIPT_DIR/Dockerfile.faucet -t linera-faucet . || exit 1
-        else
-            echo "Unsupported Architecture: $CPU_ARCH"
-            exit 1
-        fi
-    else
-        echo "Unsupported OS: $OSTYPE"
-        exit 1
-    fi
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      docker build --build-arg git_commit="$GIT_COMMIT" -f $SCRIPT_DIR/Dockerfile.faucet . -t linera-faucet || exit 1
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+      CPU_ARCH=$(sysctl -n machdep.cpu.brand_string)
+      if [[ "$CPU_ARCH" == *"Apple"* ]]; then
+          docker build --build-arg git_commit="$GIT_COMMIT" --build-arg target=aarch64-unknown-linux-gnu -f $SCRIPT_DIR/Dockerfile.faucet -t linera-faucet . || exit 1
+      else
+          echo "Unsupported Architecture: $CPU_ARCH"
+          exit 1
+      fi
+  else
+      echo "Unsupported OS: $OSTYPE"
+      exit 1
   fi
-
 fi
 
 cd $SCRIPT_DIR/..
