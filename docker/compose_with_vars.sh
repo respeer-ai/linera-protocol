20e9fc1370a500da76f164e17d03221e42ff6d2c
chen <531759321@qq.com>
2025-04-02 14:23:51 +0800

    Update run genesis

--- docker/compose_with_vars.sh
+++ docker/compose_with_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ####
-## ./compose_with_vars.sh -c 7b3ae0b6 -n 2 -v "192.168.110.101 192.168.110.102" -N 0
+## ./compose_with_vars.sh -c 7b3ae0b6
 ####
 
 LAN_IP=$( hostname -I | awk '{print $1}' )
@@ -13,8 +13,6 @@ SHARDS_PER_VALIDATOR=4
 GIT_COMMIT=main
 CREATE_WALLET=0
 COMPILE=1
-VALIDATOR_INDEX=0
-PERSISTENCE_DIR=/data/linera-project/compose/genesis
 
 options="c:C:p:W:n:v:N:"
 
@@ -22,64 +20,54 @@ while getopts $options opt; do
   case ${opt} in
     c) GIT_COMMIT=${OPTARG} ;;
     C) COMPILE=${OPTARG} ;;
-    p) PERSISTENCE_DIR=${OPTARG} ;;
     W) CREATE_WALLET=${OPTARG} ;;
-    n) NUM_VALIDATORS=${OPTARG} ;;
     v) VALIDATORS=${OPTARG} ;;
-    N) VALIDATOR_INDEX=${OPTARG} ;;
   esac
 done
 
-VALIDATORS=($VALIDATORS)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VALIDATOR_TEMPLATE_FILE="${SCRIPT_DIR}/../configuration/template/validator.toml.j2"
-DOCKER_COMPOSE_TEMPLATE_FILE="${SCRIPT_DIR}/../configuration/template/docker-compose-genesis.yml.j2"
+GENESIS_DOCKER_COMPOSE_TEMPLATE_FILE="${SCRIPT_DIR}/../configuration/template/docker-compose-genesis.yml.j2"
+FAUCET_DOCKER_COMPOSE_TEMPLATE_FILE="${SCRIPT_DIR}/../configuration/template/docker-compose-faucet.yml.j2"
 
-[ ! -d $PERSISTENCE_DIR ] && CREATE_WALLET=1
+# All generated files will be put here
+OUTPUT_DIR="${SCRIPT_DIR}/../target/output/docker"
+# Source code directory
+SOURCE_DIR="${OUTPUT_DIR}/source"
+# Genesis config directory
+GENESIS_DIR="${OUTPUT_DIR}/genesis"
+# Release directory
+TARGET_DIR="${SOURCE_DIR}/linera-protocol/target/release"
+
+[ ! -d $GENESIS_DIR ] && CREATE_WALLET=1
 
 if [ "x$CREATE_WALLET" = "x1" ]; then
     containers=`docker ps -a | grep linera-validator | awk '{ print $NF }'`
     [ "x$containers" != "x" ] && docker rm -f $containers
     volume=`docker volume list | grep linera-validator |awk '{ print $2 }'`
     [ "x$volume" != "x" ] && docker volume rm $volume
-    rm $PERSISTENCE_DIR -rf
+    rm $GENESIS_DIR -rf
 fi
 
-# All persistence data will be put here
-mkdir -p $PERSISTENCE_DIR
-
-# All generated files will be put here
-OUTPUT_DIR="${SCRIPT_DIR}/../target/output/docker"
 mkdir -p $OUTPUT_DIR
-
-# All generated config files will be put here
-CONFIG_DIR="${PERSISTENCE_DIR}/config"
-mkdir -p $CONFIG_DIR
-
-# Wallet directory
-WALLET_DIR="${PERSISTENCE_DIR}/wallet"
-mkdir -p $WALLET_DIR
-
-# Source code directory
-SOURCE_DIR="${OUTPUT_DIR}/source"
 mkdir -p $SOURCE_DIR
+mkdir -p $GENESIS_DIR
 
 if [ "x$COMPILE" = "x1" ]; then
-    # Install official linera for genesis cluster
     cd $SOURCE_DIR
-    rm linera-protocol -rf
-    git clone https://github.com/linera-io/linera-protocol.git
-    cd linera-protocol
-
-    git checkout $GIT_COMMIT
     # Get latest commit to avoid compilation for the same version
     LATEST_COMMIT=`git rev-parse HEAD`
     LATEST_COMMIT=${LATEST_COMMIT:0:10}
-    INSTALLED_COMMIT=`linera --version | grep tree | awk -F '/' '{print $7}' | awk '{print $1}'`
+    INSTALLED_COMMIT=`$TARGET_DIR/linera --version | grep tree | awk -F '/' '{print $7}' | awk '{print $1}'`
 
     if [ "x$LATEST_COMMIT" != "x$INSTALLED_COMMIT" ]; then
-        cargo install --path linera-service --features storage-service
-        cargo install --path linera-storage-service --features storage-service
+        rm linera-protocol -rf
+	git clone https://github.com/linera-io/linera-protocol.git
+	cd linera-protocol
+        git checkout $GIT_COMMIT
+
+        cp $SCRIPT_DIR/faucet-entrypoint.sh docker/
+	cargo build --release --features storage-service
     fi
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -100,112 +88,123 @@ fi
 
 cd $SCRIPT_DIR/..
 
-# Generate validator configuration from template
-VALIDATOR_FILES=()
-for i in $(seq 0 $((NUM_VALIDATORS - 1))); do
-    # All validator config will be put here
-    VALIDATOR_DIR="${PERSISTENCE_DIR}/config/$i"
-    mkdir -p $VALIDATOR_DIR
-
-    # Prometheus directory
-    PROMETHEUS_DIR="$VALIDATOR_DIR/prometheus"
-    mkdir -p $PROMETHEUS_DIR
-
-    # Grafana directory
-    GRAFANA_DIR="$VALIDATOR_DIR/grafana"
-    mkdir -p $GRAFANA_DIR
-
-
-    # Generate validator configure of i
-    echo "{
-        \"validator\": {
-            \"config_path\": \"$VALIDATOR_DIR/server.json\",
-            \"host\": \"${VALIDATORS[$i]}\",
-	    \"port\": $((19100 + i * 2)),
-	    \"metrics_port\": $((20100 + i * 2)),
-            \"pyroscope_host\": \"validator-$i-docker-pyroscope\",
-	    \"pyroscope_port\": $((4040 + i * 2)),
-            \"internal_host\": \"proxy-$i\",
-	    \"internal_port\": $((21100 + i * 2))
+# Generate validator configure
+echo "{
+    \"validator\": {
+        \"config_path\": \"$GENESIS_DIR/server.json\",
+        \"host\": \"$LAN_IP\",
+        \"port\": 19100,
+        \"metrics_port\": 20100,
+        \"pyroscope_host\": \"validator-docker-pyroscope\",
+        \"pyroscope_port\": 4040,
+        \"internal_host\": \"proxy\",
+        \"internal_port\": 21100
+    },
+    \"shards\": {
+        \"shard_1\": {
+            \"host\": \"linera-validator-shard-1\",
+            \"port\": 19100,
+            \"metrics_port\": 21100,
+            \"pyroscope_host\": \"linera-validator-docker-pyroscope\",
+            \"pyroscope_port\": 4040
+      },
+        \"shard_2\": {
+            \"host\": \"linera-validator-shard-2\",
+            \"port\": 19100,
+            \"metrics_port\": 21100,
+            \"pyroscope_host\": \"linera-validator-docker-pyroscope\",
+            \"pyroscope_port\": 4040
         },
-        \"shards\": {
-            \"shard_1\": {
-                \"host\": \"linera-validator-$i-shard-1\",
-                \"port\": $((19100 + i * 2)),
-                \"metrics_port\": $((21100 + i * 2)),
-                \"pyroscope_host\": \"linera-validator-$i-docker-pyroscope\",
-                \"pyroscope_port\": $((4040 + i * 2))
-          },
-            \"shard_2\": {
-                \"host\": \"linera-validator-$i-shard-2\",
-                \"port\": $((19100 + i * 2)),
-                \"metrics_port\": $((21100 + i * 2)),
-                \"pyroscope_host\": \"linera-validator-$i-docker-pyroscope\",
-                \"pyroscope_port\": $((4040 + i * 2))
-            },
-            \"shard_3\": {
-                \"host\": \"linera-validator-$i-shard-3\",
-                \"port\": $((19100 + i * 2)),
-                \"metrics_port\": $((21100 + i * 2)),
-                \"pyroscope_host\": \"linera-validator-$i-docker-pyroscope\",
-                \"pyroscope_port\": $((4040 + i * 2))
-            },
-            \"shard_4\": {
-                \"host\": \"linera-validator-$i-shard-4\",
-                \"port\": $((19100 + i * 2)),
-                \"metrics_port\": $((21100 + i * 2)),
-                \"pyroscope_host\": \"linera-validator-$i-docker-pyroscope\",
-                \"pyroscope_port\": $((4040 + i * 2))
-            }
+        \"shard_3\": {
+            \"host\": \"linera-validator-shard-3\",
+            \"port\": 19100,
+            \"metrics_port\": 21100,
+            \"pyroscope_host\": \"linera-validator-docker-pyroscope\",
+            \"pyroscope_port\": 4040
+        },
+        \"shard_4\": {
+            \"host\": \"linera-validator-shard-4\",
+            \"port\": 19100,
+            \"metrics_port\": 21100,
+            \"pyroscope_host\": \"linera-validator-docker-pyroscope\",
+            \"pyroscope_port\": 4040
         }
-    }" > $VALIDATOR_DIR/validator.json
+    }
+}" > $GENESIS_DIR/validator.json
 
-    jinja -d $VALIDATOR_DIR/validator.json $VALIDATOR_TEMPLATE_FILE > $VALIDATOR_DIR/validator.toml
+jinja -d $GENESIS_DIR/validator.json $VALIDATOR_TEMPLATE_FILE > $GENESIS_DIR/validator.toml
 
-    VALIDATOR_FILES+=("$VALIDATOR_DIR/validator.toml")
+# Generate genesis docker-compose.yml
+echo "{
+    \"validator\": {
+        \"validator_persistence_dir\": \"$GENESIS_DIR\",
+        \"prometheus_persistence_dir\": \"$GENESIS_DIR/prometheus\",
+        \"grafana_persistence_dir\": \"$GENESIS_DIR/grafana\",
+        \"validator_name\": \"linera-validator\",
+        \"scylla_name\": \"linera-validator-scylla\",
+        \"scylla_volume\": \"linera-validator-scylla-data\",
+        \"shard_init_name\": \"linera-validator-shard-init\",
+        \"prometheus_name\": \"linera-validator-prometheus\",
+        \"grafana_name\": \"linera-validator-grafana\",
+        \"grafana_volume\": \"linera-validator-grafana-data\",
+        \"watchtower_name\": \"linera-validator-watchtower\"
+    }
+}" > $GENESIS_DIR/docker-compose-validator.json
 
-    # Generate docker-compose.yml
-    echo "{
-        \"validator\": {
-	    \"validator_persistence_dir\": \"$VALIDATOR_DIR\",
-	    \"prometheus_persistence_dir\": \"$PROMETHEUS_DIR\",
-	    \"grafana_persistence_dir\": \"$GRAFANA_DIR\",
-	    \"validator_name\": \"linera-validator-$i\",
-	    \"validator_port\": $((19100 + $i * 2)),
-	    \"scylla_name\": \"linera-validator-$i-scylla\",
-	    \"scylla_volume\": \"linera-validator-$i-scylla-data\",
-	    \"shard_init_name\": \"linera-validator-$i-shard-init\",
-	    \"prometheus_name\": \"linera-validator-$i-prometheus\",
-	    \"prometheus_port\": $((19090 + $i * 2)),
-	    \"grafana_name\": \"linera-validator-$i-grafana\",
-	    \"grafana_port\": $((3000 + $i * 2)),
-	    \"grafana_volume\": \"linera-validator-$i-grafana-data\",
-	    \"watchtower_name\": \"linera-validator-$i-watchtower\"
-        }
-    }" > $OUTPUT_DIR/docker-compose-validator.json
+jinja -d $GENESIS_DIR/docker-compose-validator.json $GENESIS_DOCKER_COMPOSE_TEMPLATE_FILE > $GENESIS_DIR/docker-compose.yml
 
-    jinja -d $OUTPUT_DIR/docker-compose-validator.json $DOCKER_COMPOSE_TEMPLATE_FILE > $VALIDATOR_DIR/docker-compose.yml
-
-done
 
 if [ "x$CREATE_WALLET" = "x1" ]; then
 # Create configuration files.
 # * Private server states are stored in `server.json`.
 # * `committee.json` is the public description of the Linera committee.
-    linera-server generate --validators "${VALIDATOR_FILES[@]}" --committee $CONFIG_DIR/committee.json --testing-prng-seed 1
+    $TARGET_DIR/linera-server generate --validators $GENESIS_DIR/validator.toml --committee $GENESIS_DIR/committee.json --testing-prng-seed 1
 # Create configuration files for 10 user chains.
 # * Private chain states are stored in one local wallet `wallet.json`.
 # * `genesis.json` will contain the initial balances of chains as well as the initial committee.
-    linera --wallet $WALLET_DIR/wallet.json --storage rocksdb:$WALLET_DIR/client.db create-genesis-config 10 --genesis $CONFIG_DIR/genesis.json --initial-funding 100000000 --committee $CONFIG_DIR/committee.json --testing-prng-seed 2
+    $TARGET_DIR/linera --wallet $GENESIS_DIR/wallet.json --storage rocksdb:$GENESIS_DIR/client.db create-genesis-config 10 --genesis $GENESIS_DIR/genesis.json --initial-funding 100000000 --committee $GENESIS_DIR/committee.json --testing-prng-seed 2
 fi
 
 cd $SCRIPT_DIR
-VALIDATOR_DIR="${PERSISTENCE_DIR}/config/$VALIDATOR_INDEX"
-mkdir -p $VALIDATOR_DIR/grafana/provisioning
-cp provisioning/dashboards $VALIDATOR_DIR/provisioning/ -R
-cp dashboards $VALIDATOR_DIR/grafana/ -R
-cp prometheus.yml $VALIDATOR_DIR/prometheus/
-cp $CONFIG_DIR/committee.json $VALIDATOR_DIR/
-cp $CONFIG_DIR/genesis.json $VALIDATOR_DIR/
+mkdir -p $GENESIS_DIR/grafana/provisioning
+mkdir -p $GENESIS_DIR/prometheus
+cp provisioning/dashboards $GENESIS_DIR/provisioning/ -R
+cp dashboards $GENESIS_DIR/grafana/ -R
+cp prometheus.yml $GENESIS_DIR/prometheus/
 
-docker compose -f $VALIDATOR_DIR/docker-compose.yml -p linera-validator-$VALIDATOR_INDEX up --wait
+docker compose -f $GENESIS_DIR/docker-compose.yml -p linera-validator up --wait
+
+# Generate docker-compose.yml
+echo "{
+    \"faucet\": {
+        \"wallet_dir\": \"$GENESIS_DIR\"
+    }
+}" > $GENESIS_DIR/docker-compose-faucet.json
+
+jinja -d $GENESIS_DIR/docker-compose-faucet.json $FAUCET_DOCKER_COMPOSE_TEMPLATE_FILE > $GENESIS_DIR/faucet-docker-compose.yml
+
+cd $SCRIPT_DIR
+
+docker rm faucet -f
+docker compose -f $GENESIS_DIR/faucet-docker-compose.yml up --wait
+
+function generate_nginx_conf() {
+    endpoint=faucet
+    domain=faucet.respeer.ai
+
+    echo "{
+        \"service\": {
+            \"endpoint\": \"$endpoint\",
+            \"servers\": [\"localhost:8080\"],
+            \"domain\": \"$domain\",
+            \"api_endpoint\": \"$endpoint\"
+        }
+    }" > ${GENESIS_DIR}/$endpoint.nginx.json
+
+    jinja -d ${GENESIS_DIR}/$endpoint.nginx.json $NGINX_TEMPLATE_FILE > ${GENESIS_DIR}/$endpoint.nginx.conf
+    echo "cp ${GENESIS_DIR}/$endpoint.nginx.conf /etc/nginx/sites-enabled/"
+}
+
+generate_nginx_conf
+echo -e "\n\nFaucet domain"
+echo -e "	$LAN_IP api.faucet.respeer.ai"
