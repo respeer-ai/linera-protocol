c6efa9579a3abb1c7a338d82073d6a47e9360d43
Zhao KK <kikakkz@hotmail.com>
2025-04-08 03:38:23 +0000

    Correct blob query

--- linera-service/src/node_service.rs
+++ linera-service/src/node_service.rs
@@ -1454,7 +1454,7 @@ where
         let chain_id: ChainId = chain_id.parse().map_err(NodeServiceError::InvalidChainId)?;
         let application_id: ApplicationId = application_id.parse()?;
         let request = format!(
-            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"",
+            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"}",
         );
 
         let response = service
@@ -1489,7 +1489,7 @@ where
         let chain_id: ChainId = chain_id.parse().map_err(NodeServiceError::InvalidChainId)?;
         let application_id: ApplicationId = application_id.parse()?;
         let request = format!(
-            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"",
+            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"}",
         );
 
         let response = service
@@ -1524,7 +1524,7 @@ where
         let chain_id: ChainId = chain_id.parse().map_err(NodeServiceError::InvalidChainId)?;
         let application_id: ApplicationId = application_id.parse()?;
         let request = format!(
-            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"",
+            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"}",
         );
 
         let response = service
@@ -1559,7 +1559,7 @@ where
         let chain_id: ChainId = chain_id.parse().map_err(NodeServiceError::InvalidChainId)?;
         let application_id: ApplicationId = application_id.parse()?;
         let request = format!(
-            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"",
+            "{\"query\": \" query {{ fetch(blobHash: \\\"{blob_hash}\\\") }}\"}",
         );
 
         let response = service
