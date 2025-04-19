c34891d88cd9d6b5a7490f33a33994d55d699398
hong-t <th13564039482@163.com>
2025-04-19 21:33:45 +0800

    Update deploy target ingress route

---
+++ kubernetes/deploy_ingress_route.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+####
+## ./deploy_ingress_route.sh -z testnet-babbage
+####
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TEMPLATE_FILE="${SCRIPT_DIR}/../configuration/template/ingress-route.yaml.j2"
+
+CLUSTER=
+
+options="z:"
+
+while getopts $options opt; do
+  case ${opt} in
+    z) CLUSTER=${OPTARG} ;;
+  esac
+done
+
+# All generated files will be put here
+OUTPUT_DIR="${SCRIPT_DIR}/../output/k8s"
+mkdir -p $OUTPUT_DIR
+
+# All generated config files will be put here
+CONFIG_DIR="${OUTPUT_DIR}/config"
+mkdir -p $CONFIG_DIR
+
+function generate_ingress_route() {
+    endpoint=$1
+    domain=$(echo "api.${CLUSTER}.$2" | sed 's/\.\./\./g')
+    target_host=$(echo "${CLUSTER}.target.$2" | sed 's/\.\./\./g')
+    name=$(echo "${CLUSTER}.$2" | sed 's/\.\./\./g' | sed 's/\./-/g')
+    echo "{
+        \"ingress\": {
+            \"name\": \"$name\",
+            \"target_host\": \"$target_host\",
+            \"domain\": \"$domain\"
+        }
+    }" > ${CONFIG_DIR}/$endpoint.target.ingress.json
+
+    jinja -d ${CONFIG_DIR}/$endpoint.target.ingress.json $TEMPLATE_FILE > ${CONFIG_DIR}/$endpoint-target-ingress.yaml
+    kubectl apply -f ${CONFIG_DIR}/$endpoint-target-ingress.yaml
+}
+
+generate_ingress_route rpc.respeer.ai
+generate_ingress_route faucet faucet.respeer.ai
