45e1aa4025f6ae06314c547bfbf2d223c889aba0
Zhao KK <kikakkz@hotmail.com>
2025-10-12 14:02:40 +0800

    Update lock file to pass unit test

--- scripts/run_unit_tests.sh
+++ scripts/run_unit_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+cd ..
+
+export LINERA_STORAGE_SERVICE="127.0.0.1:1235"
+
+killall -9 linera-storage-server
+
 cargo run --release -p linera-storage-service -- memory --endpoint $LINERA_STORAGE_SERVICE &
 cargo build -p linera-witty-test-modules --target wasm32-unknown-unknown
-cargo test --no-default-features --features fs,macros,wasmer,rocksdb --locked -j 1
+cargo test --no-default-features --features fs,macros,wasmer,rocksdb --locked -j 1 --lib
+cargo test --no-default-features --features fs,macros,wasmer,rocksdb --locked -j 1 --bins
 
-killall -9 cargo
+killall -9 linera-storage-server
