f6d427c194d13cdd799f66fe9c61479307dba094
Zhao KK <kikakkz@hotmail.com>
2026-01-26 08:42:51 +0800

    Correct service argument

--- docker/wallet-entrypoint.sh
+++ docker/wallet-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -n "${LISTENER_SKIP_PERMISSIONLESS_CHAIN+x}" ]; then
+if [ -n "${LINERA_LISTENER_SKIP_PERMISSIONLESS_CHAIN+x}" ]; then
   LISTENER_SKIP_PERMISSIONLESS_CHAIN="--listener-skip-permissionless-chain"
 fi
 
@@ -9,7 +9,7 @@ exec ./linera \
   --wallet /wallet/wallet.json \
   --keystore /wallet/keystore.json \
   --storage rocksdb:/wallet/client.db \
-  $LISTENER_SKIP_PERMISSIONLESS_CHAIN \
   service \
+  $LISTENER_SKIP_PERMISSIONLESS_CHAIN \
   --port 8080 \
   --metrics-port 8082
