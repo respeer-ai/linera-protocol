a57f3f32d9477584f5a1df1a876f4695a32ab14b
Zhao KK <kikakkz@hotmail.com>
2026-01-31 12:02:46 +0800

    Correct og

--- linera-core/src/client/mod.rs
+++ linera-core/src/client/mod.rs
@@ -2189,7 +2189,7 @@ impl<Env: Environment> ChainClient<Env> {
             );
         }
 
-        tracing::info!(?info.requested_pending_message_bundles.length(), ?self.options.max_pending_message_bundles, "pending message bundles");
+        info!(pending_messages=?info.requested_pending_message_bundles.len(), max_processing=?self.options.max_pending_message_bundles, "pending message bundles");
 
         Ok(info
             .requested_pending_message_bundles
