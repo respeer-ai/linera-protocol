c9336a33a5e5daab55e6a4a56938e447641bf952
Zhao KK <kikakkz@hotmail.com>
2026-03-23 16:13:48 +0800

    Fix docker compilation

--- docker/compose_rpc.sh
+++ docker/compose_rpc.sh
@@ -57,7 +57,7 @@ GIT_COMMIT=$(git rev-parse --short HEAD)
 
 if [ $COMPILE -eq 1 ]; then
   docker rmi linera-respeer
-  docker build --no-cache --build-arg all_proxy=$all_proxy --build-arg git_commit="$GIT_COMMIT" --build-arg build_features="scylladb,metrics,memory-profiling,tempo,disable-native-rpc,enable-wallet-rpc" -f docker/Dockerfile . -t linera-respeer || exit 1
+  docker build --no-cache --build-arg all_proxy=$all_proxy --build-arg git_commit="$GIT_COMMIT" --build-arg build_features="scylladb,metrics,memory-profiling,disable-native-rpc,enable-wallet-rpc" -f docker/Dockerfile . -t linera-respeer || exit 1
 fi
 
 export PATH=$RESPEER_BIN_DIR:$PATH
